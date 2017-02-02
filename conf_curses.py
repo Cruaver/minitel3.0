3bc252bdea0e33cc910be02474dc308ef2b15989
cruaver <charbel.kabro@gmail.com>
2017-02-02 16:04:04 +0100

    disigne teste

--- conf_curses.py
+++ conf_curses.py
@@ -8,6 +8,7 @@ def init_curses():#{
     screen = curses.initscr()
     curses.noecho()
     curses.cbreak
+    curses.start_color()
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_CYAN)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_CYAN)
     curses.init_pair(3, curses.COLOR_RED, curses.COLOR_CYAN)
