6986ed9e61e53b7adbdb4508b9c1fd4330bf48d9
Cruaver <charbel.kabro@gmail.com>
2017-02-04 09:35:27 +0100

    modif indent

--- proc.py
+++ proc.py
@@ -31,49 +31,49 @@ def kill_process():#{
     dirs = os.listdir("/proc")
     for pid in dirs:#{
     	try:#{
-        	pidDel = input("PID du processus à Kill : ")
-        	if psutil.pid_exists(pidDel):#{
-        		try:#{
-        			name = get_pname(pidDel)
-        			p = psutil.Process(pidDel)
-        			p.terminate()
-        			print "Vous avez kill le processus :", name.rstrip('\n')
-        			break
+            pidDel = input("PID du processus à Kill : ")
+            if psutil.pid_exists(pidDel):#{
+        	try:#{
+        	    name = get_pname(pidDel)
+        	    p = psutil.Process(pidDel)
+        	    p.terminate()
+        	    print "Vous avez kill le processus :", name.rstrip('\n')
+        	    break
                 #}
-        		except psutil.AccessDenied:
-        			print "Vous n'avez pas les droits pour kill le processus", name.rstrip('\n')
+               	except psutil.AccessDenied:
+        	    print "Vous n'avez pas les droits pour kill le processus", name.rstrip('\n')
             #}
-        	else:
-        		print "Aucun processus avec le pid \"{}\" en cours.".format(pidDel)
+            else:
+        	print "Aucun processus avec le pid \"{}\" en cours.".format(pidDel)
         except (NameError, TypeError):
-        	print "Vous n'avez pas entrer un nombre entier."
+            print "Vous n'avez pas entrer un nombre entier."
         #}
     #}
     return (0)
 #}
 
 def statuts_process():#{
-	try:#{
-		pidStat = input("PID du processus pour voir plus de détails : ")
-		if psutil.pid_exists(pidStat):#{
-  			pStat = psutil.Process(pidStat)
-  			parentId = psutil.Process(pidStat).ppid()
-  			print "PPID :", (parentId)
-  			print "PID :", (pidStat)
-  			print "Username :", pStat.username()
-  			try:#{
-  				print pStat.cwd()
-  				print "Cmd name :", get_pname(pidStat).rstrip('\n')
-  				print "Statut :", pStat.status()
-  				print "CPU usage :", (pStat.cpu_percent(interval=1)), "%"
-  				print "Memories usage :", round((pStat.memory_percent()), 1), "%"
-            #}
-  		    	except psutil.AccessDenied:
-  		    		print "Vous n'avez pas les droits sur ce processus pour voir plus de détails"
-  		else:
-  			print "Aucun processus avec le PID \"{}\" en cours.".format(pidStat)
-	#}
+    try:#{
+    pidStat = input("PID du processus pour voir plus de détails : ")
+    if psutil.pid_exists(pidStat):#{
+  	pStat = psutil.Process(pidStat)
+  	parentId = psutil.Process(pidStat).ppid()
+	print "PPID :", (parentId)
+  	print "PID :", (pidStat)
+        print "Username :", pStat.username()
+  	try:#{
+  	    print pStat.cwd()
+  	    print "Cmd name :", get_pname(pidStat).rstrip('\n')
+  	    print "Statut :", pStat.status()
+  	    print "CPU usage :", (pStat.cpu_percent(interval=1)), "%"
+  	    print "Memories usage :", round((pStat.memory_percent()), 1), "%"
+        #}
+  	except psutil.AccessDenied:
+  	    print "Vous n'avez pas les droits sur ce processus pour voir plus de détails"
+    else:
+        print "Aucun processus avec le PID \"{}\" en cours.".format(pidStat)
+    #}
     except (NameError, TypeError):
-		print "Vous n'avez pas entrer un nombre entier."
-  	return (0)
+        print "Vous n'avez pas entrer un nombre entier."
+    return (0)
 #}
