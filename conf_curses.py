ee1551d107c4a538c7484e8bfc4f6be3f7ee909b
root <root@debian>
2017-02-04 04:10:33 -0600

    lol

--- conf_curses.py
+++ conf_curses.py
@@ -11,6 +11,8 @@ def init_curses():#{
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLUE)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
     curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLUE)
+    curses.cbreak()
+    curses.noecho()
     screen.keypad(True)
     screen.clear()
     screen.border(0)
